23c7f89579ee1230e456909f04ca90a1f0e2f274
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-09-15 16:37:45 +0200

    wip add tp2

---
+++ TP1/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = let
+          python = pkgs.python313.withPackages (ppkgs:
+            with ppkgs; [
+              python-lsp-server
+              uv
+              pydantic
+              fastapi-cli
+              fastapi
+              joblib
+              streamlit
+              pandas
+              scikit-learn
+              mlflow
+            ]);
+        in
+          pkgs.mkShell {
+            buildInputs = [
+              python
+            ];
+            UV_PYTHON = python;
+          };
+      }
+    );
+}
---
+++ TP2/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "MLflow Dev Shell with Python and NumPy support";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default =
+          (pkgs.buildFHSEnv {
+            name = "mlflow-shell";
+
+            targetPkgs = pkgs:
+              with pkgs; [
+                python3
+                zlib
+                libffi
+                stdenv.cc.cc.lib # for libstdc++
+                git
+              ];
+          }).env;
+      }
+    );
+}
